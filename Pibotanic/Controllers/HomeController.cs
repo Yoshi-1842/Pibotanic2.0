b24a9f9a76e5fc297d963bdb288125efc1c6c558
Jonathan Vega <vegajonathan5614@gmail.com>
2021-06-03 22:24:38 -0500

    cambios de metodo de pago

--- Pibotanic/Controllers/HomeController.cs
+++ Pibotanic/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Pibotanic.Controllers
             return View();
         }
 
+        public IActionResult Metododepago()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
